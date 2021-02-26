43d2b25dc314eaa963fdefe2972e3f8cb736f402
j65864ap <andrii.perehuda@student.manchester.ac.uk>
2021-02-26 10:58:27 +0000

    Wire VenueRepository to VenueServiceImpl
    Wired VenueRepository to VenueServiceImpl and implemented save() method

--- src/main/java/uk/ac/man/cs/eventlite/dao/VenueServiceImpl.java
+++ src/main/java/uk/ac/man/cs/eventlite/dao/VenueServiceImpl.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 
@@ -16,39 +17,26 @@ import uk.ac.man.cs.eventlite.entities.Venue;
 
 @Service
 public class VenueServiceImpl implements VenueService {
+	@Autowired
+	private VenueRepository venueRepository;
 
 	private final static Logger log = LoggerFactory.getLogger(VenueServiceImpl.class);
 
 	private final static String DATA = "data/venues.json";
 
 	@Override
+	public Venue save(Venue entity) {
+		return venueRepository.save(entity);
+	}
+	
+	@Override
 	public long count() {
-		long count = 0;
-		Iterator<Venue> i = findAll().iterator();
-
-		for (; i.hasNext(); count++) {
-			i.next();
-		}
-
-		return count;
+		return venueRepository.count();
 	}
 
 	@Override
 	public Iterable<Venue> findAll() {
-		Iterable<Venue> venues;
-
-		try {
-			ObjectMapper mapper = new ObjectMapper();
-			InputStream in = new ClassPathResource(DATA).getInputStream();
-
-			venues = mapper.readValue(in, mapper.getTypeFactory().constructCollectionType(List.class, Venue.class));
-		} catch (Exception e) {
-			// If we can't read the file, then the event list is empty...
-			log.error("Exception while reading file '" + DATA + "': " + e);
-			venues = Collections.emptyList();
-		}
-
-		return venues;
+		return venueRepository.findAll();
 	}
 
 }
