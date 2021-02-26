718cf5811fb947f247d255ee63e57c02ee855974
j65864ap <andrii.perehuda@student.manchester.ac.uk>
2021-02-26 11:05:03 +0000

    Remove unnecessary imports in VenueServiceImpl

--- src/main/java/uk/ac/man/cs/eventlite/dao/VenueServiceImpl.java
+++ src/main/java/uk/ac/man/cs/eventlite/dao/VenueServiceImpl.java
@@ -1,17 +1,8 @@
 package uk.ac.man.cs.eventlite.dao;
 
-import java.io.InputStream;
-import java.util.Collections;
-import java.util.Iterator;
-import java.util.List;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 import uk.ac.man.cs.eventlite.entities.Venue;
 
@@ -19,10 +10,6 @@ import uk.ac.man.cs.eventlite.entities.Venue;
 public class VenueServiceImpl implements VenueService {
 	@Autowired
 	private VenueRepository venueRepository;
-
-	private final static Logger log = LoggerFactory.getLogger(VenueServiceImpl.class);
-
-	private final static String DATA = "data/venues.json";
 
 	@Override
 	public Venue save(Venue entity) {
