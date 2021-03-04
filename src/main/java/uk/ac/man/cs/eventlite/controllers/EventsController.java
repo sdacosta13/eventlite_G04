ec15bfd22bcb1fbfe182d6712376c8a60ee43ab1
Harry Freeborough <harry.freeborough@student.manchester.ac.uk>
2021-03-04 17:44:11 +0000

    Add delete button to event index

--- src/main/java/uk/ac/man/cs/eventlite/controllers/EventsController.java
+++ src/main/java/uk/ac/man/cs/eventlite/controllers/EventsController.java
@@ -4,11 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import uk.ac.man.cs.eventlite.dao.EventService;
 import uk.ac.man.cs.eventlite.dao.VenueService;
+import uk.ac.man.cs.eventlite.entities.Event;
 
 @Controller
 @RequestMapping(value = "/events", produces = { MediaType.TEXT_HTML_VALUE })
@@ -25,4 +29,10 @@ public class EventsController {
 		return "events/index";
 	}
 
+	@DeleteMapping("/{event}")
+	public String deleteEvent(@PathVariable Event event, RedirectAttributes redirectAttrs) {
+		eventService.delete(event);
+		redirectAttrs.addFlashAttribute("ok_message", "Greeting deleted.");
+		return "redirect:/events";
+	}
 }
