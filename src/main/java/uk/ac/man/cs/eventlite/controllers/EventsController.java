d09d0c1d919dae332ea3a1554746922748052685
j65864ap <andrii.perehuda@student.manchester.ac.uk>
2021-03-04 17:04:13 +0000

    Implement search event by name functionality

--- src/main/java/uk/ac/man/cs/eventlite/controllers/EventsController.java
+++ src/main/java/uk/ac/man/cs/eventlite/controllers/EventsController.java
@@ -5,7 +5,9 @@ import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import uk.ac.man.cs.eventlite.dao.EventService;
 import uk.ac.man.cs.eventlite.dao.VenueService;
@@ -18,11 +20,14 @@ public class EventsController {
 	private EventService eventService;
 
 	@GetMapping
-	public String getAllEvents(Model model) {
-
-		model.addAttribute("events", eventService.findAll());
+	public String getAllEvents(@RequestParam(value = "name", required = false) String name, Model model) {
+		
+		if (name == null) {
+			model.addAttribute("events", eventService.findAll());
+		} else {
+			model.addAttribute("events", eventService.findAllByNameContaining(name));
+		}
 
 		return "events/index";
 	}
-
 }
