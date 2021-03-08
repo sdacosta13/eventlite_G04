55ba25157165c115aa65068300e00096a0b12615
Sam da Costa <samuel.dacosta-2@student.manchester.ac.uk>
2021-03-08 08:02:00 +0000

    Add event adding functionality
    Currently unfinished, stuck on bug when submitting post request

--- src/main/java/uk/ac/man/cs/eventlite/controllers/EventsController.java
+++ src/main/java/uk/ac/man/cs/eventlite/controllers/EventsController.java
@@ -1,14 +1,21 @@
 package uk.ac.man.cs.eventlite.controllers;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import uk.ac.man.cs.eventlite.dao.EventService;
 import uk.ac.man.cs.eventlite.dao.VenueService;
+import uk.ac.man.cs.eventlite.entities.Event;
+import uk.ac.man.cs.eventlite.entities.Venue;
 
 @Controller
 @RequestMapping(value = "/events", produces = { MediaType.TEXT_HTML_VALUE })
@@ -16,6 +23,8 @@ public class EventsController {
 
 	@Autowired
 	private EventService eventService;
+	@Autowired
+	private VenueService venueService;
 
 	@GetMapping
 	public String getAllEvents(Model model) {
@@ -24,5 +33,19 @@ public class EventsController {
 
 		return "events/index";
 	}
-
+	
+	@GetMapping(value="/addEvent")
+	public String addEvent(Model model) {
+		model.addAttribute("venues", venueService.findAll());
+		model.addAttribute("event", new Event());
+		return "events/addEvent";
+	}
+	
+	@PostMapping(value="events/eventSubmit")
+	@ResponseBody
+	public String eventSubmit(@ModelAttribute(value="event") Event event) {
+		eventService.save(event);
+		return "events/index";
+	}
+	
 }
