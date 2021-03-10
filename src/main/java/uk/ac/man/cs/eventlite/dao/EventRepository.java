d7528fa5f4c85cbf35599395ff7ed18c1bcc6bf9
Audric Sudirman <audric.sudirman@student.manchester.ac.uk>
2021-03-10 10:33:12 +0000

    Create dummy page for event info
    Added functionality to database interface to get event information by
    their ID, which is obtained with the URL and passed this information to
    the webpage via a newly made EventPageController.

--- src/main/java/uk/ac/man/cs/eventlite/dao/EventRepository.java
+++ src/main/java/uk/ac/man/cs/eventlite/dao/EventRepository.java
@@ -6,4 +6,5 @@ import uk.ac.man.cs.eventlite.entities.Event;
 public interface EventRepository extends CrudRepository<Event, Long> {
 	public Iterable<Event> findAllByOrderByDateAsc();
 	public Iterable<Event> findAllByNameContaining(String infix);
+	public Event findById(long id);
 }
