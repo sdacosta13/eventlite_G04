4cfd81d2fabde508da998b163d7bb8b0fa09e241
j65864ap <andrii.perehuda@student.manchester.ac.uk>
2021-03-15 16:35:21 +0000

    Search Event by multiple keywords

--- src/main/java/uk/ac/man/cs/eventlite/dao/EventServiceImpl.java
+++ src/main/java/uk/ac/man/cs/eventlite/dao/EventServiceImpl.java
@@ -48,9 +48,52 @@ public class EventServiceImpl implements EventService {
 		return eventRepository.findAllContainingAlternativeIgnoreCaseOrderByNameAscDateAsc(prefix, suffix, infix, full);
 	}
 	
-	@Override
-	public Iterable<Event> findAllFromSearch(String name) {
+	@Override	
+	public Iterable<Event> findAllSingleKeyword(String name) {
 		return findAllContainingAlternativeIgnoreCaseOrderByNameAscDateAsc(name + " %", "% " + name, "% " + name + " %", name);
+	}
+	
+	@Override	
+	public Iterable<Event> findAllFromSearch(String name) {
+		return findAllMultipleKeywords(name);
+	}
+	
+	@Override
+	public Iterable<Event> findAllMultipleKeywords(String name){
+		String[] keywords = name.split("\\s+");
+		if (keywords.length == 0) return new ArrayList<Event>();
+		
+		Object[] results = new Object[keywords.length];
+		for (int i = 0; i < keywords.length; i++) {
+			results[i] = findAllFromSearch(keywords[i]);
+		}
+		ArrayList<Event> returnList = new ArrayList<Event>();
+		Iterator<Event> iterator =((Iterable<Event>) results[0]).iterator();
+		while(iterator.hasNext()) {
+			boolean found = true;
+			Event e = iterator.next();
+			for (int i = 1; i < keywords.length; i++) {
+				if (!search(e, ((Iterable<Event>) results[i]))) {
+					found = false;
+					break;
+				}
+			}
+			if (found) {
+				returnList.add(e);
+			}
+		}
+		
+		return returnList;
+	}
+	
+	private boolean search(Event e, Iterable<Event> list) {
+		Iterator<Event> iterator = list.iterator();
+		while(iterator.hasNext()) {
+			if (iterator.next() == e) {
+				return true;
+			}
+		}
+		return false;
 	}
 	
 	public Event findById(long id) {
