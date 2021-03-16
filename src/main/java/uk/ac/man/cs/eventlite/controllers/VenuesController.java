f2eae200c6c6b2d9dd6c8907293d76bdcb04864e
Harry Freeborough <harry.freeborough@student.manchester.ac.uk>
2021-03-16 20:42:38 +0000

    Add venues controllers

---
+++ src/main/java/uk/ac/man/cs/eventlite/controllers/VenuesController.java
@@ -0,0 +1,22 @@
+package uk.ac.man.cs.eventlite.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import uk.ac.man.cs.eventlite.dao.VenueService;
+
+@Controller
+@RequestMapping(value = "/venues", produces = { MediaType.TEXT_HTML_VALUE })
+public class VenuesController {
+	@Autowired
+	private VenueService venueService;
+
+	@GetMapping
+	public String getAllVenues(Model model) {
+		model.addAttribute("venues", venueService.findAll());
+		return "venues/index";
+	}
+}
