7631a06ca44d1155744ab20694227c2a02703fce
Jovan <jovan.petreski@student.manchester.ac.uk>
2021-03-22 00:08:43 +0000

    Add annotation to fields
    Annotations added to set constraints for venue attributes.

--- src/main/java/uk/ac/man/cs/eventlite/entities/Venue.java
+++ src/main/java/uk/ac/man/cs/eventlite/entities/Venue.java
@@ -5,6 +5,9 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
 
 
 @Entity
@@ -15,12 +18,18 @@ public class Venue {
 	@GeneratedValue(strategy=GenerationType.AUTO)
 	private long id;
 
+	@NotEmpty(message = "Venue name is required.")
+	@Size(max = 255, message = "Name length must not exceed 255 characters.")
 	private String name;
 
+	@Positive(message = "Must be positive integer.")
 	private int capacity;
 
+	@NotEmpty(message = "Address is required.")
+	@Size(max = 299, message = "Road name must be less than 300 characters long.")
 	private String address;
 
+	@NotEmpty(message = "Postcode is required.")
 	private String postcode;
 	
 	public Venue() {
