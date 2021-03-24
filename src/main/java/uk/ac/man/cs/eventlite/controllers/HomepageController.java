d288a454a0146d41b46592ada6bb5b005699cf8e
Audric Sudirman <audric.sudirman@student.manchester.ac.uk>
2021-03-24 21:42:04 +0000

    Add list of upcoming events to homepage

--- src/main/java/uk/ac/man/cs/eventlite/controllers/HomepageController.java
+++ src/main/java/uk/ac/man/cs/eventlite/controllers/HomepageController.java
@@ -1,5 +1,8 @@
 package uk.ac.man.cs.eventlite.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
@@ -10,13 +13,36 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import uk.ac.man.cs.eventlite.dao.EventService;
 import uk.ac.man.cs.eventlite.dao.VenueService;
+import uk.ac.man.cs.eventlite.entities.Event;
 
 @Controller
 @RequestMapping(value = "/", produces = { MediaType.TEXT_HTML_VALUE })
 public class HomepageController {
 	
+	@Autowired
+	private EventService eventService;
+	
 	@GetMapping
 	public String getAllVenues(Model model) {
+		Iterable<Event> events = eventService.findAll();
+		
+		// Should probably replace with getting top 3 results rather than processing all of them here
+		int count = 0;
+		List<Event> upcomingEvents = new ArrayList<>();
+		for (Event e : events) {
+			if (e.isPast()) {
+				continue;
+			}
+			else {
+				upcomingEvents.add(e);
+				count++;
+			}
+			if (count >= 3) {
+				break;
+			}
+		}
+		
+		model.addAttribute("upcomingEvents", upcomingEvents);
 		return "homepage/index";
 	}
 
