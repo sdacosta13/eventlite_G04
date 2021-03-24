3527380da1512a9ce6b0da00a10252110dc5e8a6
Audric Sudirman <audric.sudirman@student.manchester.ac.uk>
2021-03-24 21:15:54 +0000

    Create Homepage
    Migrated index to use Thymeleaf and added folder path for images.
    Changed look of homepage to look a little nicer and include space for
    the required info.

---
+++ src/main/java/uk/ac/man/cs/eventlite/controllers/HomepageController.java
@@ -0,0 +1,23 @@
+package uk.ac.man.cs.eventlite.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import uk.ac.man.cs.eventlite.dao.EventService;
+import uk.ac.man.cs.eventlite.dao.VenueService;
+
+@Controller
+@RequestMapping(value = "/", produces = { MediaType.TEXT_HTML_VALUE })
+public class HomepageController {
+	
+	@GetMapping
+	public String getAllVenues(Model model) {
+		return "homepage/index";
+	}
+
+}
