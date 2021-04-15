c5b7cd24349fbe6110bef7bd330797a1a86972f1
Audric Sudirman <audric.sudirman@student.manchester.ac.uk>
2021-04-15 18:00:11 +0100

    Use Twitter API to tweet message from event info page
    Currently cannot obtain message from form

--- src/main/java/uk/ac/man/cs/eventlite/controllers/EventPageController.java
+++ src/main/java/uk/ac/man/cs/eventlite/controllers/EventPageController.java
@@ -1,14 +1,24 @@
 package uk.ac.man.cs.eventlite.controllers;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import twitter4j.Status;
+import twitter4j.TwitterException;
 import uk.ac.man.cs.eventlite.dao.EventService;
+import uk.ac.man.cs.eventlite.services.TwitterService;
 
 @Controller
 @RequestMapping(value = "event/{eventId}", produces = { MediaType.TEXT_HTML_VALUE })
@@ -17,10 +27,32 @@ public class EventPageController {
 	@Autowired
 	private EventService eventService;
 	
+	@Autowired
+	private TwitterService twitterService;
+	
 	@GetMapping
 	public String getEventInfo(Model model, @PathVariable Long eventId) {
 		model.addAttribute("event", eventService.findById(eventId));
+		model.addAttribute("message", "");
 		return "events/info-page";
+	}
+	
+	@PostMapping(value="/tweetSubmit")
+	public String tweetSubmit(@RequestBody @Valid @ModelAttribute String message, RedirectAttributes redirectAttrs) {
+		try {
+//			message = "Test Message";
+			System.out.println("Print debuggingaaaaaaaaaaaaaaaaaaaaaaa");
+			System.out.println(message);
+			twitterService.getTwitter().updateStatus(message);
+			redirectAttrs.addFlashAttribute("ok_message", String.format("Your tweet: '%s' was posted", message));
+			
+		} catch (TwitterException e) {
+			redirectAttrs.addFlashAttribute("error_message", "Twitter is not available.");
+			e.printStackTrace();
+		}
+		
+		return "redirect:/events";
+		
 	}
 
 }
